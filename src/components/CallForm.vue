8d17911d605f034d5aee4b9c8153d4d82d35ddfd
JordiGis <jorgisfer@alu.edu.gva.es>
2025-02-20 18:23:18 +0100

    ADD type call

--- src/components/CallForm.vue
+++ src/components/CallForm.vue
@@ -6,7 +6,8 @@
 
         <div class="form-group">
           <label>Paciente:</label>
-          <Field as="select" v-model="call.patientId" name="patientId" class="form-control" :disabled="!call.incoming && alert.patientId ">
+          <Field as="select" v-model="call.patientId" name="patientId" class="form-control"
+            :disabled="!call.incoming && alert.patientId">
             <option v-for="patient in patients" :key="patient.id" :value="patient.id">
               {{ patient.name }} {{ patient.lastName }}
             </option>
@@ -32,7 +33,7 @@
 
         <div v-if="call.incoming === false" class="form-group">
           <label>Información de Alerta:</label>
-          
+
           <div class="flex justify-center items-center min-h-screen bg-gray-100">
             <form class="bg-white shadow-lg rounded-2xl p-6 w-full max-w-md">
               <h2 class="text-xl font-semibold text-gray-700 mb-4">Detalles de la Alerta</h2>
@@ -40,22 +41,26 @@
               <div class="space-y-4">
                 <div>
                   <label class="block text-sm font-medium text-gray-600">Tipo</label>
-                  <input type="text" :value="alert.type" disabled class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
+                  <input type="text" :value="alert.type" disabled
+                    class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
                 </div>
 
                 <div>
                   <label class="block text-sm font-medium text-gray-600">Subtipo</label>
-                  <input type="text" :value="alert.subType" disabled class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
+                  <input type="text" :value="alert.subType" disabled
+                    class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
                 </div>
 
                 <div>
                   <label class="block text-sm font-medium text-gray-600">Descripción</label>
-                  <textarea :value="alert.description" disabled class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500"></textarea>
+                  <textarea :value="alert.description" disabled
+                    class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500"></textarea>
                 </div>
 
                 <div>
                   <label class="block text-sm font-medium text-gray-600">Fecha de Inicio</label>
-                  <input type="text" :value="alert.startDate" disabled class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
+                  <input type="text" :value="alert.startDate" disabled
+                    class="w-full mt-1 p-2 border rounded-lg bg-gray-100 text-gray-500" />
                 </div>
               </div>
             </form>
@@ -65,32 +70,29 @@
             <option :value="true">Entrante</option>
             <option :value="false">Saliente</option>
           </Field> -->
-          <div class="form-group">
-          <label>Tipo:</label>
-          <Field name="selectedType" v-slot="{ field, errors }" rules="required">
-            <select v-bind="field" v-model="call.type" @change="onTypeChange">
-              <option value="" disabled>Selecciona un tipo</option>
-              <option v-for="type in callTypes" :key="type.name" :value="type">
-                {{ type.spanishName }}
-              </option>
-            </select>
-            <span class="error-message" v-if="errors.length">{{ errors[0] }}</span>
+        <div class="form-group">
+          <label for="type">Tipo</label>
+          <Field as="select" name="type" v-model="call.type" class="form-control" :validate="validateType">
+            <option value="" disabled>Selecciona un tipo</option>
+            <option v-for="type in callTypes" :key="type.name" :value="type">
+              {{ type.spanishName }}
+            </option>
           </Field>
+          <ErrorMessage class="error" name="type" />
         </div>
 
         <div class="form-group">
-          <label>SubTipo:</label>
-          <Field name="selectedSubtype" v-slot="{ field, errors }" rules="required">
-            <select v-bind="field" v-model="selectedSubtype">
-              <option value="" disabled>Selecciona un subtipo</option>
-              <option v-for="subtype in filteredSubtypes" :key="subtype.id" :value="subtype">
-                {{ subtype.spanishName }}
-              </option>
-            </select>
-            <span class="error-message" v-if="errors.length">{{ errors[0] }}</span>
+          <label for="subType">Subtipo</label>
+          <Field as="select" name="subType" v-model="call.subType" class="form-control" :disabled="!call.type"
+            :validate="validateSubType">
+            <option value="" disabled>Selecciona un subtipo</option>
+            <option v-for="subtype in filteredSubtypes" :key="subtype.id" :value="subtype">
+              {{ subtype.spanishName }}
+            </option>
           </Field>
+          <ErrorMessage class="error" name="subType" />
         </div>
-          
+
 
         <div class="form-group">
           <label>Duración:</label>
@@ -138,7 +140,7 @@ export default {
     await this.loadAlerts();
     await this.loadUsers();
     await this.loadUser();
-    await this.loadCallTypes(); 
+    await this.loadCallTypes();
     const user = JSON.parse(localStorage.getItem("user"));
     this.loggedUser = user ? user.name : "Usuario";
     if (this.id) {
@@ -155,7 +157,14 @@ export default {
       this.alert.subType = this.alert.subType;
       this.alert.description = this.alert.description;
       this.alert.startDate = this.alert.startDate;
-      this.call.incoming =false;
+      this.call.incoming = false;
+    }
+
+    if (this.call.type) {
+      this.call.type = this.callTypes.find(type => type.name === this.call.type);
+    }
+    if (this.call.subType) {
+      this.call.subType = this.call.type.subtypes.find(subtype => subtype.name === this.call.subType);
     }
   },
   computed: {
@@ -164,6 +173,7 @@ export default {
       return this.id ? 'Editar llamada' : 'Añadir llamada';
     },
     filteredSubtypes() {
+      // Filtra los subtipos basados en el tipo seleccionado
       return this.call.type ? this.call.type.subtypes : [];
     }
   },
@@ -172,8 +182,8 @@ export default {
       mySchema: yup.object({
         patientId: yup.string().required('Paciente es obligatorio'),
         incoming: yup.boolean().required('Tipo de llamada es obligatorio'),
-        selectedType: yup.string().required('Tipo de llamada es obligatorio'),
-        selectedSubtype: yup.string().required('SubTipo de llamada es obligatorio'),
+        // selectedType: yup.string().required('Tipo de llamada es obligatorio'),
+        // selectedSubtype: yup.string().required('SubTipo de llamada es obligatorio'),
         duration: yup.number().positive('Duración debe ser positiva').required('Duración es obligatoria'),
         description: yup.string().required('Descripción es obligatoria'),
       }),
@@ -204,18 +214,19 @@ export default {
   methods: {
     ...mapActions(useDataStore, ['loadPatients', 'loadUsers', 'loadUser', 'loadAlerts', 'loadCallTypes']),
     async handleSubmit() {
-      console.log('Formulario enviado:', this.call);
-      const call = { 
+      debugger
+      const call = {
         patientId: this.call.patientId,
         userId: this.user.id,
         incoming: this.call.incoming,
-        type: this.call.type,
-        subType: this.call.subType,
+        type: this.call.type.name,
+        subType: this.call.subType.name,
         date: new Date().toISOString().slice(0, 19).replace('T', ' '),
         duration: this.call.duration,
         description: this.call.description,
         alertId: (this.call.alertId === '') ? null : this.call.alertId,
       };
+      console.log('Formulario enviado:', call);
       try {
         if (this.id) {
           await axios.put(`${API}/calls/${this.id}`, call);
@@ -243,6 +254,19 @@ export default {
     handleCancel() {
       this.$router.push('/calls');
     },
+    validateType(value) {
+      return yup.string().required('Este campo es obligatorio').validate(value);
+    },
+    validateSubType(value) {
+      return yup.string().required('Este campo es obligatorio').validate(value);
+    },
+    onTypeChange() {
+      this.call.subType = null; // Resetea el subtipo cuando cambia el tipo
+    },
+    // handleSubmit(values) {
+    //   // Manejar el envío del formulario
+    //   console.log('Formulario enviado', values);
+    // }
   },
   watch: {
     'call.incoming'() {
@@ -250,6 +274,16 @@ export default {
         this.call.patientId = this.alert.patientId;
       }
     },
+    preselectedType(newValue) {
+      if (newValue && newValue.name) {
+        // Al cambiar el tipo preseleccionado, asignamos el tipo y el subtipo
+        const selectedType = this.callTypes.find(type => type.name === newValue.name);
+        if (selectedType) {
+          this.call.type = selectedType;
+          this.call.subType = selectedType.subtypes.find(subtype => subtype.id === this.preselectedSubtype.id);
+        }
+      }
+    }
   },
 };
 </script>
@@ -293,8 +327,8 @@ Form {
 
 .form-control,
 .form-control-plaintext,
- select,
- textarea{
+select,
+textarea {
   width: 100%;
   max-width: 250px;
   padding: 6px 10px;
